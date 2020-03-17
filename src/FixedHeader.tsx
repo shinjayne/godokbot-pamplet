9e2019e58b20addf64b8642410c6151f498b6077
Jayne Shin <shinjayne@gmail.com>
2020-03-17 14:17:57 +0900

    fixed header 가 420 px 이상일 때에도 타이틀 잘 나오도록

--- src/FixedHeader.tsx
+++ src/FixedHeader.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Title from "antd/lib/typography/Title";
 import {primaryColor} from "./color";
-import {Icon} from "antd";
+import styled from "styled-components";
 
 interface IProps {
   toggled?: boolean,
@@ -10,29 +10,11 @@ interface IProps {
 }
 
 
-const FixedHeader : React.FC<IProps> = ({toggled = false, title, subTitle}) => {
+const FixedHeader: React.FC<IProps> = ({toggled = false, title, subTitle}) => {
 
   return (
     <>
-      <div style={{
-        position: 'fixed',
-        left: 0,
-        top: 0,
-        color: toggled ? primaryColor : 'white',
-        backgroundColor:  toggled ? 'white': 'transparent',
-        width: '100%',
-
-        zIndex: 1,
-        boxShadow: toggled ? "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" : "",
-        padding: 16,
-
-        height: toggled ? 60 : 40,
-
-        transitionDelay:'0s',
-        transitionDuration:'0.35s',
-        transitionProperty:'all',
-
-      }}>
+      <FixedContainer toggled={String(toggled)}>
         <Title level={1} style={{
           fontWeight: 1000,
           fontSize: toggled ? 25 : 30,
@@ -43,16 +25,41 @@ const FixedHeader : React.FC<IProps> = ({toggled = false, title, subTitle}) => {
           width: "fit-content",
           // border: toggled ? '' : `3px solid ${toggled ? primaryColor : 'white'}`,
 
-          transitionDelay:'0s',
-          transitionDuration:'0.35s',
-          transitionProperty:'all',
+          transitionDelay: '0s',
+          transitionDuration: '0.35s',
+          transitionProperty: 'all',
         }}> {title}</Title>
         <span style={{fontSize: 14}}>
-        {toggled &&  subTitle}
+        {toggled && subTitle}
         </span>
-      </div>
+      </FixedContainer>
     </>
   );
 };
+
+
+const FixedContainer = styled.div<{ toggled: string }>`
+        position: fixed;
+        left: 0;
+        top: 0;
+        color: ${({toggled}) => toggled === 'true' ? primaryColor : 'white'};
+        background-color:  ${({toggled}) => toggled === 'true' ? 'white' : 'transparent'};
+        width: 100%;
+
+        z-index: 1;
+        box-shadow: ${({toggled}) => toggled === 'true' ? '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' : ''};
+        
+        padding: 16px;
+
+        height: ${({toggled}) => toggled === 'true' ? '60px' : '40px'};
+
+        transition-delay:0s;
+        transition-duration:0.35s;
+        transition-property:all;
+        
+        @media screen and (min-width: 420px) {
+          color: ${primaryColor};
+        }
+`;
 
 export default FixedHeader;
--- src/color.ts
+++ src/color.ts
@@ -1,6 +1,6 @@
-export const  primaryColor = 'rgb(70, 23, 143)';
+export const  primaryColor = 'rgb(239,133,44)';
 export const textColor = 'rgb(69, 79, 93)';
-export const buttonColor = 'rgb(70, 23, 143)';
+export const buttonColor = 'rgb(239,107,31)';
 
 export const pointRed = '#e01d59';
-export const pointYellow = '#ebb12d';
+export const pointYellow = '#ef852c';
